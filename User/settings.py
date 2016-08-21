0a7c466faa7ec5c77e398e1f865b750b52c2c07f
Scruf <ekozitski@hotmail.com>
2016-08-21 11:24:57 -0400

    Testing app

--- User/settings.py
+++ User/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'Test.apps.TestConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
