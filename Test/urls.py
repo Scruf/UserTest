f39f6c826b2b865a63c6ec10eb9e30abbe58ed60
Scruf <ekozitski@hotmail.com>
2016-08-21 11:52:36 -0400

    Added regiser

--- Test/urls.py
+++ Test/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^validate', views.validate, name='validate')
+	url(r'^validate', views.validate, name='validate'),
+	url(r'^register', views.register, name='register'),
+	url(r'^save', views.save, name='save')
 ]
--- Test/views.py
+++ Test/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import loader
+from .models import User
 # Create your views here.
 def index(request):
 	index_template = loader.get_template('Test/index.html')
@@ -9,3 +10,16 @@ def index(request):
 def validate(request):
 	if request.method == 'POST':
 		return HttpResponse("You Poster AssWipe")
+
+def register(request):
+	if request.method == 'POST':
+		email = request.POST.get('email')
+		password = request.POST.get('password')
+		user = User(email=email,password=password)
+		user.save()
+		return redirect ('/')
+
+def save(request):
+	save_template = loader.get_template('Test/register.html')
+	return HttpResponse(save_template.render(None, request))
+
