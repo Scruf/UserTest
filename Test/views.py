1cc4baba81f875bb9f6270a5f9f1de0ef3913586
Scruf <ekozitski@hotmail.com>
2016-08-21 17:02:30 +0000

    Added verification

--- Test/views.py
+++ Test/views.py
@@ -9,7 +9,11 @@ def index(request):
 
 def validate(request):
 	if request.method == 'POST':
-		return HttpResponse("You Poster AssWipe")
+	    try:
+                user = User.objects.get(email=request.POST.get('email'), password=request.POST.get('password'))
+                return HttpResponse('Success')
+            except User.DoesNotExists:
+                return HttpResponse('Failure')
 
 def register(request):
 	if request.method == 'POST':
