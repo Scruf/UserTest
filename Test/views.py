d260fec9704c417726d1f87967e076def136ff9c
Scruf <ekozitski@hotmail.com>
2016-08-21 11:39:11 -0400

    Added Post

--- Test/views.py
+++ Test/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello From Index')
+	index_template = loader.get_template('Test/index.html')
+	return HttpResponse(index_template.render(None,request))
 
+def validate(request):
+	if request.method == 'POST':
+		return HttpResponse("You Poster AssWipe")
