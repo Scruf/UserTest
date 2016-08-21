3a468623752daf3695cc740e63ae4d3d41c7045e
Scruf <ekozitski@hotmail.com>
2016-08-21 13:05:56 -0400

    Fixed merge issues

--- Test/views.py
+++ Test/views.py
@@ -9,20 +9,12 @@ def index(request):
 
 def validate(request):
 	if request.method == 'POST':
-<<<<<<< HEAD
 		try:
-			user = User.objects.get(email=request.POST.get('email'))
-			return HttpResponse("Success")
+			user = User.objects.get(email=request.POST.get('email'), password=request.POST.get('password'))
+			return HttpResponse('Success')
 		except User.DoesNotExists:
-			return HttpResponse("Failure")
-	
-=======
-	    try:
-                user = User.objects.get(email=request.POST.get('email'), password=request.POST.get('password'))
-                return HttpResponse('Success')
-            except User.DoesNotExists:
-                return HttpResponse('Failure')
->>>>>>> 1cc4baba81f875bb9f6270a5f9f1de0ef3913586
+			return HttpResponse('Failure')
+
 
 def register(request):
 	if request.method == 'POST':
